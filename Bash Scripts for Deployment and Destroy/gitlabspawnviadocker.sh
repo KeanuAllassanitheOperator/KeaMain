4fc38d87c0eaf39a5c706dbec3ba07de06b4b9c5
Keanu Allassani <keanuallassani@gmail.com>
2022-04-03 10:26:39 +0000

    Update gitlabspawnviadocker.sh

--- Bash Scripts for Deployment and Destroy/gitlabspawnviadocker.sh
+++ Bash Scripts for Deployment and Destroy/gitlabspawnviadocker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 docker-compose -f keagitlab.yaml up -d 
 docker restart gitlab-ce
-echo "Gitlab running as container on Port 8083"
+IPMaster=$(hostname -I | cut -d' ' -f1)
+echo "Gitlab running as container on Port 8083 on $IPMaster"
