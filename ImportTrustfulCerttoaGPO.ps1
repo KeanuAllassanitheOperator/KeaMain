a321f1836bafc91f9b7a7559a51b5f768c5834cf
Keanu Allassani <keanuallassani@gmail.com>
2022-04-14 18:27:39 +0000

    Update ImportTrustfulCerttoaGPO.ps1

--- ImportTrustfulCerttoaGPO.ps1
+++ ImportTrustfulCerttoaGPO.ps1
@@ -1,7 +1,8 @@
 # Dieses Skript importiert ein Win Server Zertfikat als Vertrauenswürdige Zertifikat, damit Client diesem vertrauen
-$certsGpoName = 'RadiusGPO'
+#  Mit dir Cert:\LocalMachine\my kann man seine aktuellen Zertifikate sehen
+$certsGpoName = 'Name of GPO'
 
-$certThumbprint = '69A2F2ED59000C57719A210CF728F790533BDF6E'
+$certThumbprint = 'Thumbprint des Certs'
 
 $certRegistryKeyPath = 'HKLM:\SOFTWARE\Microsoft\SystemCertificates\MY\Certificates\{0}' -f $certThumbprint
 
