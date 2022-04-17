e1bffce956af98d167bebf9ecb7f9539239baf9c
Keanu Allassani <keanuallassani@gmail.com>
2022-04-17 14:18:02 +0000

    Update showlwanpasswords(german).ps1

--- showlwanpasswords(german).ps1
+++ showlwanpasswords(german).ps1
@@ -1,6 +1,1 @@
-netsh wlan show networks
-netsh wlan show profile SSIDName key=clear | Select-String "Schlüsselinhalt"
-
-netsh wlan show profile SSIDName key=clear | Select-String "Key Content"
-
-(netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} | Format-Table -AutoSize 
+(netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Schlüsselinhalt\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} | Format-Table -AutoSize
