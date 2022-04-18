7b6bf4afd6802fa9debaac57ffc616e5b2c2a364
Keanu Allassani <keanuallassani@gmail.com>
2022-04-18 21:30:42 +0000

    Update UninstallDC.ps1

--- Active Directory /UninstallDC.ps1
+++ Active Directory /UninstallDC.ps1
@@ -2,5 +2,9 @@ Remove-WindowsFeature AD-Domain-Services
 Remove-WindowsFeature ADRMS 
 Remove-WindowsFeature  ADLDS 
 Remove-WindowsFeature  ADFS-Federation
+Remove-Windows-Feature RSAT-ADDS
+Remove-Windows-Feature RSAT-AD-AdminCenter
+Remove-Windows-Feature   RSAT-ADDS-Tools
+Remove-WindowsFeature    RSAT-ADLDS
 Uninstall-WindowsFeature ADCS-Cert-Authority -IncludeManagementTools
 Restart-Computer
