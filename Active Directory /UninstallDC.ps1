a281c7f9b0384fc0bb7305e51211b3d9bb1dc9aa
Keanu Allassani <keanuallassani@gmail.com>
2022-04-18 22:24:11 +0000

    Update UninstallDC.ps1

--- Active Directory /UninstallDC.ps1
+++ Active Directory /UninstallDC.ps1
@@ -1,4 +1,3 @@
-Remove-WindowsFeature AD-Domain-Services
 Remove-WindowsFeature ADRMS 
 Remove-WindowsFeature  ADLDS 
 Remove-WindowsFeature  ADFS-Federation
@@ -6,6 +5,7 @@ Remove-WindowsFeature RSAT-ADDS
 Remove-WindowsFeature RSAT-AD-AdminCenter
 Remove-WindowsFeature   RSAT-ADDS-Tools
 Remove-WindowsFeature    RSAT-ADLDS
+Remove-Item  -Path cert:\LocalMachine\My\FE387EA22C4A2A61E276A526C177ADE382104A92 -DeleteKey
 Uninstall-WindowsFeature ADCS-Cert-Authority -IncludeManagementTools
-Uninstall-ADDSDomainController -LastDomainControllerInDomain -LocalAdministratorPassword (ConvertTo-SecureString -AsPlainText "YourAdminPassword" -Force) -RemoveApplicationPartitions
-Restart-Computer
+Uninstall-ADDSDomainController -LastDomainControllerInDomain -LocalAdministratorPassword (ConvertTo-SecureString -AsPlainText "passwordfromyou" -Force) -RemoveApplicationPartitions
+Remove-WindowsFeature AD-Domain-Services
