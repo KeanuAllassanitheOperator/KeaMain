6ecbec5d23b2522452b1368a25b6571f15c8feb2
Keanu Allassani <keanuallassani@gmail.com>
2022-04-21 19:51:43 +0000

    Upload New File

---
+++ Active Directory/AddADUsers.ps1
@@ -0,0 +1,6 @@
+﻿#Listenobjekt oder Datei#
+$liste = @('')
+foreach ($i in $liste) { 
+New-ADUser -Name $i -AccountPassword $pw -Passwordneverexpires $true -Enabled $true
+Add-ADGroupMember -Identity "Domänen-Admins" -Members $domainuser
+}
