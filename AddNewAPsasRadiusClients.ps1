a46d3a8ff8430b8c8ef0bbca3327e1e648b1473a
Keanu Allassani <keanuallassani@gmail.com>
2022-04-21 21:20:44 +0000

    Deleted InstallNPS.ps1, AddNewAPsasRadiusClients.ps1

--- AddNewAPsasRadiusClients.ps1
+++
@@ -1,9 +0,0 @@
-# Liste der IPS von den APs
-$listeap = @('')
-$secret = 
-$listeip = @('')
-foreach ($a in $listeap) {
- foreach ($i in $listeip) {
-New-NpsRadiusClient -Address $i -Name $a -SharedSecret $secret
- }
-}
