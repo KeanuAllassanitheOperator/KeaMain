afa66b5c23d941a118b5761bab95ffdbba41430c
Keanu Allassani <keanuallassani@gmail.com>
2022-04-21 19:52:25 +0000

    Upload New File

---
+++ Active Directory/AddDomainAdminUser.ps1
@@ -0,0 +1,12 @@
+﻿$domainuser = Read-Host -Prompt 'Enter a user, who will become a DomainAdmin' 
+$pw = Read-Host -Prompt 'Enter a Password for this user' -AsSecureString 
+New-ADUser -Name $domainuser -AccountPassword $pw -Passwordneverexpires $true -Enabled $true
+Add-ADGroupMember -Identity "Domänen-Admins" -Members $domainuser
+# Schreibzugriff auf C Volume klappt nicht
+$acl = Get-Acl C:
+
+$AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule("KEADC0\Keanu","FullControl","Allow")
+
+$acl.SetAccessRule($AccessRule)
+
+$acl | Set-Acl C:
