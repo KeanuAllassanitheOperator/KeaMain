d82bf20450fbc5cf747ec604e22f664a214d69f6
Keanu Allassani <keanuallassani@gmail.com>
2022-04-25 14:28:18 +0000

    Add new file

---
+++ Exchange/uninstallexchange2019.ps1
@@ -0,0 +1,3 @@
+Get-Mailbox | Disable-Mailbox
+Get-Mailbox -Arbitration | Disable-Mailbox -Arbitration -DisableLastArbitrationMailboxAllowed
+Setup /Mode:Uninstall /IAcceptExchangeServerLicenseTerms
