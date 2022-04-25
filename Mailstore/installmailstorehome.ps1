6c7cadae44315d49101ba7c1364f991012e28695
Keanu Allassani <keanuallassani@gmail.com>
2022-04-25 20:57:52 +0000

    Update installmailstorehome.ps1

--- Mailstore/installmailstorehome.ps1
+++ Mailstore/installmailstorehome.ps1
@@ -1,2 +1,3 @@
-﻿Invoke-WebRequest -OutFile "MailStoreHomeSetup-22.1.0.20583.exe" https://download.mailstore.com/dont-link-this/36B44217-2BF0-4603-8160-35DF2A708DC3/22.1.0.20583/MailStoreHomeSetup-22.1.0.20583.exe  
+#https://www.mailstore.com/en/products/mailstore-home/
+Invoke-WebRequest -OutFile "MailStoreHomeSetup-22.1.0.20583.exe" https://download.mailstore.com/dont-link-this/36B44217-2BF0-4603-8160-35DF2A708DC3/22.1.0.20583/MailStoreHomeSetup-22.1.0.20583.exe  
 Start-Process "MailStoreHomeSetup-22.1.0.20583.exe" -ArgumentList "/passive" -wait -Passthru
