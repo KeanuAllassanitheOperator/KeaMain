270cbb438fdbdfa1d077defa6c2936bf546d5296
Keanu Allassani <keanuallassani@gmail.com>
2022-04-25 20:31:38 +0000

    Update DistributionGroupCreate.ps1

--- Exchange/DistributionGroupCreate.ps1
+++ Exchange/DistributionGroupCreate.ps1
@@ -1,2 +1,3 @@
-﻿New-DistributionGroup -Name 'Strohhutpiraten' -Alias 'Mugiwara' -Type 'Security'
+#https://www.windowspro.de/roland-eich/verteilergruppen-fuer-exchange-powershell-anlegen-verwalten
+New-DistributionGroup -Name 'Strohhutpiraten' -Alias 'Mugiwara' -Type 'Security'
 Add-DistributionGroupMember -Identity "Strohhutpiraten" -Member "ruffy@keadc.local"
