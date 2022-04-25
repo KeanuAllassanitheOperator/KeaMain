dff6e34469a7a6747802e943b62f503232ebdf46
Keanu Allassani <keanuallassani@gmail.com>
2022-04-25 20:57:08 +0000

    Upload New File

---
+++ Mailstore/installmailstorehome.ps1
@@ -0,0 +1,2 @@
+﻿Invoke-WebRequest -OutFile "MailStoreHomeSetup-22.1.0.20583.exe" https://download.mailstore.com/dont-link-this/36B44217-2BF0-4603-8160-35DF2A708DC3/22.1.0.20583/MailStoreHomeSetup-22.1.0.20583.exe  
+Start-Process "MailStoreHomeSetup-22.1.0.20583.exe" -ArgumentList "/passive" -wait -Passthru
