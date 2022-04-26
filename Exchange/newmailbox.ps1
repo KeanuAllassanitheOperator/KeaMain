a2af009da9a522cfce7d07c67a449677460a023b
Keanu Allassani <keanuallassani@gmail.com>
2022-04-26 13:18:25 +0000

    Update newmailbox.ps1

--- Exchange/newmailbox.ps1
+++ Exchange/newmailbox.ps1
@@ -1,4 +1,5 @@
 Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapI
+Add-Type -AssemblyName System.Web
+$password = [System.Web.Security.Membership]::GeneratePassword(10,1)
 $mailboxlist = @('')
-$password = Read-Host "Enter password" -AsSecureString
 New-Mailbox -UserPrincipalName "ruffy@keadc.local" -Database "EX2019-DB01" -Name "Monkey D Ruffy" -OrganizationalUnit "Users" -Password $password -FirstName "Monkey D" -LastName "Ruffy" -DisplayName "Monkey D Ruffy" -Alias "ruffy"
