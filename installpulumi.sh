05dc58aae4538aaca83de96be33a04d7b089acc4
Keanu Allassani <keanuallassani@gmail.com>
2022-04-27 14:31:09 +0000

    Add new file

---
+++ installpulumi.sh
@@ -0,0 +1,4 @@
+#https://www.pulumi.com/docs/get-started/
+#https://www.pulumi.com/?utm_source=google&utm_medium=cpc&utm_campaign=&utm_term=pulumi&utm_medium=ppc&utm_source=adwords&hsa_grp=132900039961&hsa_cam=15160582074&hsa_mt=e&hsa_net=adwords&hsa_ver=3&hsa_acc=1926559913&hsa_ad=588955657872&hsa_src=g&hsa_tgt=kwd-1177947170384&hsa_kw=pulumi&gclid=Cj0KCQjw06OTBhC_ARIsAAU1yOWRSsN8T_uXHZ6mG9UgfhH2l3iNUequyZl66QHcumDpFx2ADQWqo2MaAkDFEALw_wcB
+#!/bin/bash
+curl -fsSL https://get.pulumi.com | sh
