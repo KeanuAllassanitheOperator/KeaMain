e101480d347179387b3a5e059fab18f9600b9b10
Keanu Allassani <keanuallassani@gmail.com>
2022-04-27 12:54:52 +0000

    Upload New File

---
+++ DFS/CreateNewNetwrokFolders.ps1
@@ -0,0 +1,3 @@
+﻿mkdir C:\DFSRoots\Resources
+New-SmbShare –Name "Resources" –Path "C:\DFSRoots\Resources" -FullAccess "KeaDC0\Administrator"
+New-DfsnRoot -Path '\\keadc.local\Resources' -Type DomainV2 -TargetPath '\\keadc\Resources'
