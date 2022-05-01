5559905785e3b42d9682bad3caf7263b324140b1
Keanu Allassani <keanuallassani@gmail.com>
2022-05-01 16:28:07 +0000

    Update installansible.sh

--- installansible.sh
+++ installansible.sh
@@ -7,7 +7,6 @@ sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose
 sudo usermod -aG docker $USER
 sudo systemctl restart docker
-sudo systemctl status docker
 docker version
 docker-compose version
 sudo apt install -y ansible
@@ -16,8 +15,9 @@ sudo apt install -y nodejs npm
 sudo npm install npm --global
 wget https://github.com/ansible/awx/archive/17.1.0.zip
 sudo unzip 17.1.0.zip
-cd awx-17.1.0 /installer
+cd awx-17.1.0/installer
 DecryptKey=$( pwgen -N 1 -s 30)
 sed 's/# admin_password=password/admin_password=ansible1234#/g'
 sed 's/secret_key=awxsecret/secret_key=$DecryptKey/g'
 ansible-playbook -i inventory install.yml
+sudo systemctl status docker
