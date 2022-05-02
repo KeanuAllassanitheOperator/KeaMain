d65627cadda29e9be7159d1f5eca6051505a7e2e
Keanu Allassani <keanuallassani@gmail.com>
2022-05-02 11:23:22 +0000

    Update installansible.sh

---
+++ installoperatortools.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt update -y
+sudo apt install ansible -y
+sudo mkdir ansible-playbooks
+sudo apt install vagrant -y
+sudo apt install ruby-dev build-essentials libmysqlclient-dev -y
+gem install nokogiri --platform=ruby
+vagrant plugin install vagrant-vsphere vagrant-env 
