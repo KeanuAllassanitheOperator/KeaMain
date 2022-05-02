d7b41c77c762ed95268eeafa362368060c489c97
Keanu Allassani <keanuallassani@gmail.com>
2022-05-02 15:05:17 +0000

    Update installoperatortools.sh

--- installoperatortools.sh
+++ installoperatortools.sh
@@ -8,3 +8,4 @@ gem install nokogiri --platform=ruby
 vagrant plugin install vagrant-vsphere vagrant-env
 sudo ansible-galaxy collection install kubernetes.core
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+echo "/srv/nfs/kubedata*(rw,sync,no_subtree_check,no_root_squash,no_all_squash,insecure)" > /etc/exports
