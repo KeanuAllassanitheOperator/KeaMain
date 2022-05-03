5f3871a6c6331ee5b5af8849aba629f3771f9bb2
Keanu Allassani <keanuallassani@gmail.com>
2022-05-03 20:33:15 +0000

    Update AddNewAPsasRadiusClients.ps1

--- Active Directory/AddNewAPsasRadiusClients.ps1
+++ Active Directory/AddNewAPsasRadiusClients.ps1
@@ -1,6 +1,14 @@
 # Liste der IPS von den APs
 $ap = Read-Host("Name des Access Points ")
 $secret = 
-$ip = Read-Host("IP-Adresse des Radius Clients ")
-New-NpsRadiusClient -Address $i -Name $a -SharedSecret $secret
-Get-NpsRadiusClient
+do {
+ $ip = Read-Host("IP-Adresse des Radius Clients ")
+ $ipregex = $ipv4 = '^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$'
+ if ($ip -match $ipregex) {
+  New-NpsRadiusClient -Address $i -Name $a -SharedSecret $secret 
+  Get-NpsRadiusClient
+  break
+ else {
+   Write-Host ($ip isnt a valid IP Address)
+ }
+} while ($ip -notmatch $ipregex)   
