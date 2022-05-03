9b3a8ca0b2ce11d8fb38ec3183cb0e08976ef3dd
Keanu Allassani <keanuallassani@gmail.com>
2022-05-03 20:25:58 +0000

    Update AddNewAPsasRadiusClients.ps1

--- Active Directory/AddNewAPsasRadiusClients.ps1
+++ Active Directory/AddNewAPsasRadiusClients.ps1
@@ -1,5 +1,6 @@
 # Liste der IPS von den APs
-$ap = Read-Host ("Name des Access Points")
+$ap = Read-Host("Name des Access Points")
 $secret = 
+$ip = Read-Host("IP-Adresse des Radius Clients")
 New-NpsRadiusClient -Address $i -Name $a -SharedSecret $secret
 Get-NpsRadiusClient
