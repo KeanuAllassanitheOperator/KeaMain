a01e204ab505940eef64f0dbe074d964f70eb57a
Keanu Allassani <keanuallassani@gmail.com>
2022-05-04 12:33:51 +0000

    Update installoperatortools.sh

--- installoperatortools.sh
+++ installoperatortools.sh
@@ -3,6 +3,7 @@ sudo apt install git -y
 sudo apt install ansible -y
 sudo mkdir ansible-playbooks
 sudo apt install vagrant -y
+sudo apt install toilet -y 
 sudo apt install ruby-dev build-essential libmysqlclient-dev rubygems -y
 gem install nokogiri --platform=ruby
 vagrant plugin install vagrant-env vagrant-vsphere
