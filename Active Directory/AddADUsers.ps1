69fd595b78734b5d150ee964ad5cebdf5619911d
Keanu Allassani <keanuallassani@gmail.com>
2022-05-05 19:27:10 +0000

    Update Active Directory/AddADUsers.ps1

--- Active Directory/AddADUsers.ps1
+++ Active Directory/AddADUsers.ps1
@@ -1,8 +1,15 @@
 Add-type -AssemblyName System.Web
-$Username = Read-Host("Legen Sie einen neuen Benutzer an: ") 
-$Group = Read-Host("Zu welcher Gruppe soll der Benutzer zugewiesen werden ? ")
-$password = [System.Web.Security.Membership]::GeneratePassword(12,1)
-New-ADUser -Name $Username -AccountPassword $password -Passwordneverexpires $true -Enabled $true
-Add-ADGroupMember -Identity $Group -Members $Username
-Write-Host  $Username + " " + $password  
-
+$userlist = @('')
+$group 
+foreach $i in $userlist {
+  if Get-ADUser -Filter { SamAccountName -eq $SamAccountName } 
+  {
+      "User exists."
+  }  
+  else {
+        $password = [System.Web.Security.Membership]::GeneratePassword(12,1)
+        New-ADUser -Name $i -AccountPassword $password -SamAccountName $i -Passwordneverexpires $true -Enabled $true
+        Add-ADGroupMember -Identity $group -Members $i
+        Write-Host  $i + " " + $password  
+      }
+}
