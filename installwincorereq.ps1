5ed64d8accc9007c0cca3e199b1196034f1b9919
Keanu Allassani <keanuallassani@gmail.com>
2022-05-06 22:00:58 +0000

    Update installwincorereq.ps1

--- installwincorereq.ps1
+++ installwincorereq.ps1
@@ -6,3 +6,4 @@ Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0 
 Set-Service -Name sshd -StartupType ‘Automatic’ 
 Start-Service ssh
+w32tm /config /manualpeerlist:"0.de.pool.ntp.org 1.de.pool.ntp.org 2.de.pool.ntp.org" /syncfromflags:manual /reliable:yes /update
