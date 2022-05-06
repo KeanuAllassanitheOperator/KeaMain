866b90fb59fc738b451d3b0cac9a247208db4741
Keanu Allassani <keanuallassani@gmail.com>
2022-05-06 21:45:19 +0000

    Update installwincorereq.ps1

--- Active Directory/installwincorereq.ps1
+++ Active Directory/installwincorereq.ps1
@@ -1,5 +1,7 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; ` iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install ntop
+choco install nano
+choco install vim
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0 
 Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0 
 Set-Service -Name sshd -StartupType ‘Automatic’ 
