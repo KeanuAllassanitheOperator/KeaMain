768477034aee73d675d761d088e93dd4520faef7
Keanu Allassani <keanuallassani@gmail.com>
2022-06-03 14:18:04 +0000

    Update SetUnlimitedCharLength.ps1

--- Windows Server Allgemein/SetUnlimitedCharLength.ps1
+++ Windows Server Allgemein/SetUnlimitedCharLength.ps1
@@ -1,1 +1,1 @@
-Set-ItemProperty 'HKLM:\System\CurrentControlSet\Control\FileSystem' -Name 'LongPathsEnabled' -v
+Set-ItemProperty 'HKLM:\System\CurrentControlSet\Control\FileSystem' -Name 'LongPathsEnabled' -value 1
