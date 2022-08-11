1794a7742c44fc79f21f396ab7b624ac7a609c45
Keanu Allassani <keanuallassani@gmail.com>
2022-08-11 07:27:01 +0000

    Update configureansibleforwindows.ps1

--- configureansibleforwindows.ps1
+++ configureansibleforwindows.ps1
@@ -1,5 +1,5 @@
 $url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
 $file = "$env:temp\ConfigureRemotingForAnsible.ps1"
 (New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
-powershell.exe -Set-ExecutionPolicy -ExecutionPolicy ByPass -File $file -EnableCredSSP -ForceNewSSLCert
+Set-ExecutionPolicy -ExecutionPolicy ByPass -File $file -EnableCredSSP -ForceNewSSLCert
 Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
