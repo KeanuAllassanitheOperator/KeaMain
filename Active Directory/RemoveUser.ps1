3cc484ccbffdacd516ac03cb434a462d8b43a8b9
Keanu Allassani <keanuallassani@gmail.com>
2022-10-16 17:40:39 +0000

    Update RemoveUser.ps1

--- Active Directory/RemoveUser.ps1
+++ Active Directory/RemoveUser.ps1
@@ -1,4 +1,6 @@
 $User = @('')
+# Nach deaktivierten Konten filtern und löschen
+#Search-ADAccount -AccountDisabled | where {$_.ObjectClass -eq 'user'} | Remove-ADUser
 foreach $i in $User{ 
 Remove-ADUser -Identity $i
 } 
