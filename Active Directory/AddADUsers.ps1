4cae651a383a139a3d9e44af7c0abd5df3f0ed38
Keanu Allassani <keanuallassani@gmail.com>
2022-10-16 17:30:55 +0000

    Update AddADUsers.ps1

--- Active Directory/AddADUsers.ps1
+++ Active Directory/AddADUsers.ps1
@@ -1,15 +1,36 @@
-Add-type -AssemblyName System.Web
-$userlist = @('')
-$group 
-foreach $i in $userlist {
-  if Get-ADUser -Filter { SamAccountName -eq $SamAccountName } 
-  {
-      "User exists."
-  }  
-  else {
-        $password = [System.Web.Security.Membership]::GeneratePassword(12,1)
-        New-ADUser -Name $i -AccountPassword $password -SamAccountName $i -Passwordneverexpires $true -Enabled $true
-        Add-ADGroupMember -Identity $group -Members $i
-        Write-Host  $i + " " + $password  
-      }
+#Enter a path to your import CSV file
+$ADUsers = Import-csv "C:\PSScripts\CSVDumps\newusers.csv"
+
+foreach ($User in $ADUsers)
+{
+
+       $Username    = $User.Username
+       $Password    = $User.Passwort
+       $Firstname   = $User.Vorname
+       $Lastname    = $User.Nachname
+       $OU           = $User.ou
+
+       #Check if the user account already exists in AD
+       if (Get-ADUser -F {SamAccountName -eq $Username})
+       {
+               #If user does exist, output a warning message
+               Write-Warning "A user account $Username has already exist in Active Directory."
+       }
+       else
+       {
+              #If a user does not exist then create a new user account
+          
+        #Account will be created in the OU listed in the $OU variable in the CSV file; don’t forget to change the domain name in the"-UserPrincipalName" variable
+              New-ADUser `
+            -SamAccountName $Username `
+            -UserPrincipalName "$Username@bktm.local" `
+            -Name "$Firstname $Lastname" `
+            -GivenName $Firstname `
+            -Surname $Lastname `
+            -Enabled $True `
+            -DisplayName "$Lastname, $Firstname" `
+            -Path $OU `
+            -AccountPassword (convertto-securestring $Password -AsPlainText -Force)
+
+       }
 }
