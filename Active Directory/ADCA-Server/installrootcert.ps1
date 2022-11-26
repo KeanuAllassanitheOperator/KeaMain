9edbbc3fe277947258cddfeed60b125dad13397a
Keanu Allassani <keanuallassani@gmail.com>
2022-11-26 14:20:42 +0000

    Add new file

---
+++ Active Directory/ADCA-Server/installrootcert.ps1
@@ -0,0 +1,15 @@
+#https://learn.microsoft.com/en-us/powershell/module/adcsdeployment/install-adcscertificationauthority?view=windowsserver2022-ps
+$CAType="StandaloneRootCA" 
+$CAName="blabla"
+$CADistinguishedNameSuffix=DC=,DC=”
+$CryptoProviderName=“RSA#Microsoft Software Key Storage Provider”
+$KeyLength=4096
+$HashAlgorithmName=SHA1
+$ValidityPeriod=Years
+$ValidityPeriod=10
+$DBDir=“C:\windows\system32\certLog”
+$LogDir=“c:\windows\system32\CertLog”
+
+Install-ADcsCertificationAuthority -CAType $CAType –CACommonName $CAName –CADistinguishedNameSuffix $CADistinguishedNameSuffix –CryptoProviderName $CryptoProviderName -KeyLength $KeyLength –HashAlgorithmName $HashAlgorithmName –ValidityPeriod $ValidityPeriod –ValidityPeriodUnits $ValidityPeriod –DatabaseDirectory $DBDir –LogDirectory $LogDir –Force
+  
+
