9adc34bba6570b360f19ca1f6cb90ac771595ec5
Keanu Allassani <keanuallassani@gmail.com>
2022-12-21 10:20:06 +0000

    Update configureansibleforwindows.ps1

--- configureansibleforwindows.ps1
+++ configureansibleforwindows.ps1
@@ -1,4 +1,4 @@
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0 
 Set-Service -Name sshd -StartupType ‘Automatic’ 
-Start-Service ssh
+Start-Service sshd
 Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
