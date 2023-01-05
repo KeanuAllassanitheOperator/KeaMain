d2b2290c074435d13ce5a9c7696230938a40aa23
Keanu Allassani <keanuallassani@gmail.com>
2023-01-05 15:21:08 +0000

    Update 3 files
    - /mayan.yml
    - /MayanDMS/mayanedms.yml
    - /MayanDMS/downloadreqfiles.sh

---
+++ MayanDMS/downloadreqfiles.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl https://gitlab.com/mayan-edms/mayan-edms/-/raw/master/docker/docker-compose.yml -O
+curl https://gitlab.com/mayan-edms/mayan-edms/-/raw/master/docker/env_file -O
+curl https://gitlab.com/mayan-edms/mayan-edms/-/raw/master/docker/.env -O
