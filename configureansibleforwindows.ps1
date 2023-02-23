86af7d80cd438c074060362ea364e178b6ea7a1f
Keanu Allassani <keanuallassani@gmail.com>
2023-02-23 12:57:39 +0000

    Update configureansibleforwindows.ps1

--- configureansibleforwindows.ps1
+++ configureansibleforwindows.ps1
@@ -1,5 +1,3 @@
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0 
 Start-Service sshd
 Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 0
-Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
