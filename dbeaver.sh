8e7e385e3692117c5bc54b25643f69845aacdaa8
Keanu Allassani <keanuallassani@gmail.com>
2023-08-24 19:06:32 +0000

    Update 4 files
    - /phpmyadminkea.yaml
    - /postgresadminfrontend.yaml
    - /microsoftsql.yml
    - /dbeaver.sh

--- dbeaver.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Open Source DB Management Tool für Entwickler unterstützt in der Open Source Edition sich mit verschienen DB-Servern zu verbinden. 
-# CLick House, Db2 IBM, Derby Server, Firebird, H2 Embedded, Trino, PSQL, Oracle, Mysql, MariaDB, Oracle, MSSQL, SQLite, Db2 
-# Redis, NoSQL, AWS und Neo4j, Graphql kosten Geld EE Image
-docker run --name cloudbeaver-ce -d --restart unless-stopped -p 8080:8978 -v /var/cloudbeaver/workspace:/opt/cloudbeaver/workspace dbeaver/cloudbeaver:latest
