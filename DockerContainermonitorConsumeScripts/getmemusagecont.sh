935523dc77c36e902bea986473f2f77cc774aa27
Keanu Allassani <keanuallassani@gmail.com>
2023-08-28 18:09:59 +0000

    Update getmemusagecont.sh

--- DockerContainermonitorConsumeScripts/getmemusagecont.sh
+++ DockerContainermonitorConsumeScripts/getmemusagecont.sh
@@ -9,7 +9,7 @@ do
      mem=$(docker stats --no-stream --format "table {{.Name}}\t{{.CPUPerc}}\t{{.MemUsage}}" | grep $c | rev | cut -d" " -f3 | rev | sed 's/MiB//g' | cut -d"." -f1)
      #echo $mem
      memmb=$(($mem * 1024 * 1024 / 1000 / 1000 ))
-     echo $memmb
+     #echo $memmb
      if [[ $anzahl -gt 0 ]]; then echo -n "|" >> $output; fi
      echo -n "$c=$mem" >> $output
      anzahl=1
