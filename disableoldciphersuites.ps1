ceb350f3f6acb958ab8b0b8277d2d71f66566e9d
Keanu Allassani <keanuallassani@gmail.com>
2023-09-05 18:46:22 +0000

    Update disableoldciphersuites.ps1

--- disableoldciphersuites.ps1
+++ disableoldciphersuites.ps1
@@ -41,3 +41,137 @@ New-ItemProperty `
     | Out-Null
 
 Write-Host 'SSL 2.0 has been disabled.'
+
+New-Item `
+    -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 3.0\Server' `
+    -Force `
+    | Out-Null
+
+New-ItemProperty `
+    -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 3.0\Server' `
+    -name 'Enabled' `
+    -value '0' `
+    -PropertyType 'DWord' `
+    -Force `
+    | Out-Null
+
+New-ItemProperty `
+    -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 3.0\Server' `
+    -name 'DisabledByDefault' `
+    -value 1 `
+    -PropertyType 'DWord' `
+    -Force `
+    | Out-Null
+
+New-Item `
+    -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 3.0\Client' `
+    -Force `
+    | Out-Null
+
+New-ItemProperty `
+    -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 3.0\Client' `
+    -name 'Enabled' `
+    -value '0' `
+    -PropertyType 'DWord' `
+    -Force `
+    | Out-Null
+
+New-ItemProperty `
+    -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 3.0\Client' `
+    -name 'DisabledByDefault' `
+    -value 1 `
+    -PropertyType 'DWord' `
+    -Force `
+    | Out-Null
+
+Write-Host 'SSL 3.0 has been disabled.'
+
+New-Item `
+    -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Server' `
+    -Force `
+    | Out-Null
+
+New-ItemProperty `
+    -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Server' `
+    -name 'Enabled' `
+    -value '0' `
+    -PropertyType 'DWord' `
+    -Force `
+    | Out-Null
+
+New-ItemProperty `
+    -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Server' `
+    -name 'DisabledByDefault' `
+    -value 1 `
+    -PropertyType 'DWord' `
+    -Force `
+    | Out-Null
+
+New-Item `
+    -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client' `
+    -Force `
+    | Out-Null
+
+New-ItemProperty `
+    -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client' `
+    -name 'Enabled' `
+    -value '0' `
+    -PropertyType 'DWord' `
+    -Force `
+    | Out-Null
+
+New-ItemProperty `
+    -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client' `
+    -name 'DisabledByDefault' `
+    -value 1 `
+    -PropertyType 'DWord' `
+    -Force `
+    | Out-Null
+
+Write-Host 'TLS 1.0 has been disabled.'
+
+New-Item `
+    -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server' `
+    -Force `
+    | Out-Null
+
+New-ItemProperty `
+    -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server' `
+    -name 'Enabled' `
+    -value '0' `
+    -PropertyType 'DWord' `
+    -Force `
+    | Out-Null
+
+New-ItemProperty `
+    -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server' `
+    -name 'DisabledByDefault' `
+    -value 1 `
+    -PropertyType 'DWord' `
+    -Force `
+    | Out-Null
+
+New-Item `
+    -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Client' `
+    -Force `
+    | Out-Null
+
+New-ItemProperty `
+    -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Client' `
+    -name 'Enabled' `
+    -value '0' `
+    -PropertyType 'DWord' `
+    -Force `
+    | Out-Null
+
+New-ItemProperty `
+    -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Client' `
+    -name 'DisabledByDefault' `
+    -value 1 `
+    -PropertyType 'DWord' `
+    -Force `
+    | Out-Null
+
+Write-Host 'TLS 1.1 has been disabled.'
+
+
