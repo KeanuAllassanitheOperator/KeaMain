f1f466ff22e5ba7334dc0472912ddcfb320e22cd
Keanu Allassani <keanuallassani@gmail.com>
2023-10-23 18:52:53 +0000

    Update configureansibleforwindows.ps1

--- configureansibleforwindows.ps1
+++ configureansibleforwindows.ps1
@@ -2,16 +2,5 @@ Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 Start-Service sshd
 Set-Service -Name sshd -StartupType 'Automatic'
 Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-$App = 5A894077.McAfeeSecurity
-$PackageFullName = (Get-AppxPackage $App).PackageFullName
-$ProPackageFullName = (Get-AppxProvisionedPackage -online | where {$_.Displayname -eq $App}).PackageName
-if ($PackageFullName){
-    Write-Output "Removing Package: $App"
-    try {
-            remove-AppxPackage -package $PackageFullName -ErrorAction Stop | Out-Null
-        } 
-    catch {
-            Write-Output "ERROR: $_"
-        }
-        
+
     }
