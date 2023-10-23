4807e0320cfd45114bad42c39de80e9cddaf9f47
Keanu Allassani <keanuallassani@gmail.com>
2023-10-23 18:53:03 +0000

    Update configureansibleforwindows.ps1

--- configureansibleforwindows.ps1
+++ configureansibleforwindows.ps1
@@ -3,4 +3,4 @@ Start-Service sshd
 Set-Service -Name sshd -StartupType 'Automatic'
 Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-    }
+    
