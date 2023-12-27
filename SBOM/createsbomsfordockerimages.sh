697de7de3ec29a582b00d1b5be7f0d396f30af7e
Keanu Allassani <keanuallassani@gmail.com>
2023-12-27 20:13:54 +0000

    Update createsbomsfordockerimages.sh

--- SBOM/createsbomsfordockerimages.sh
+++ SBOM/createsbomsfordockerimages.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-docker image ls | cut -d " " -f1 | grep -v "REPO" | while read line; do docker sbom $line --format spdx-json --output $line.json; done
+rm -rf /tmp/sbom-*
+docker image ls | cut -d " " -f1 | grep -v "REPO" | while read line; do docker sbom $line --format cyclonedx-json --output $line.json; done
 echo "Done with analyzing Components in Docker Images"
