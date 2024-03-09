18f3e5db8031d28c2d81337f9a7c3d230f89706e
Keanu Allassani <keanuallassani@gmail.com>
2024-03-09 16:30:53 +0000

    Update gitpush.sh

--- gitpush.sh
+++ gitpush.sh
@@ -4,3 +4,4 @@ git init
 sudo git add .
 sudo git commit -m "initial commit"
 sudo git push -uf origin remotebranchname
+ git remote set-url blackjack git@gitlab.com:keanuallassani/blackjack-game.git # für ssh keys
